617e1c44c659d883c2705073612f74346fd60235
HelintonAlves <helinton-alves@hotmail.com>
2018-11-27 19:48:43 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/helintonalves/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.helintonalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helintonalves.cursomc.domain.Categoria;
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
