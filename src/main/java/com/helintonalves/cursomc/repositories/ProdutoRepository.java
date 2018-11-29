41b60099710708cfab1d38abade964dd9a56d924
HelintonAlves <helinton-alves@hotmail.com>
2018-11-29 12:16:40 -0200

    Produtos e associacao muitos para muitos

---
+++ src/main/java/com/helintonalves/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.helintonalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.helintonalves.cursomc.domain.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
