b8b6e782dec517132c9968880c73992355bac99f
HelintonAlves <helinton-alves@hotmail.com>
2018-12-03 09:33:36 -0200

    Endpoint/clientes/{id} disponivel

---
+++ src/main/java/com/helintonalves/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.helintonalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.helintonalves.cursomc.domain.Cliente;
+import com.helintonalves.cursomc.repositories.ClienteRepository;
+import com.helintonalves.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id 
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
